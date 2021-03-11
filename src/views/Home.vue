b1feac056310e3138fa2e6b8d45025a4aa7cda38
KAMAGASAKO Masatoshi <kamagasako@gmail.com>
2021-03-11 23:59:57 +0900

    ログインページ追加(Homeを更新)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <h1>recording diet</h1>
+    <form>
+      <input type="text" v-model="username" placeholder="username" />
+      <input type="password" v-model="password" placeholder="password" />
+      <button type="button" @click="login">login</button>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { http } from "@/axios.ts";
+import "url";
 
 export default defineComponent({
   name: "Home",
-  components: {
-    HelloWorld
+  data: () => {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.username);
+      const params = new URLSearchParams({
+        username: this.username,
+        password: this.password
+      });
+      http.post("/login", params);
+    }
   }
 });
 </script>
